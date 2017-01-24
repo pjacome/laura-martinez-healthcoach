8cf8ada90c0ea778e78508de542207f2d8521ec5
pablo <pabloejacome@gmail.com>
2017-01-23 19:45:34 -0800

    setting up crud files. making site live for the first time

---
+++ routes/controllers/admin.js
@@ -0,0 +1,1 @@
+﻿/* admin crud routes */
--- server.js
+++ server.js
@@ -11,28 +11,20 @@ app.set('view engine', 'handlebars');
 app.use(require('body-parser').urlencoded({extended: true}));
 app.use(require('body-parser').json());
 app.disable('x-powered-by');
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname + '/public'));
 app.use('/en', express.static(__dirname + '/public'));
 app.use('/es', express.static(__dirname + '/public'));
-// Enable this on Host Server ... but why?
-// because this is template caching
-//app.enable('view cache');
+app.enable('view cache');
 
 // routes
 app.use('/', require('./routes/routes'));
 
-// for localhost
-// app.listen(app.get('port'), function() {
-// for wifi
-var ipv4 = '192.168.1.64';
-//ipv4 = '127.0.0.1';
-
-//db.init(function(err) {
-//    if(err) {
-//        console.log(err);
-//    }
-//});
+db.init(function(err) {
+    if(err) {
+        console.log(err);
+    }
+});
 
 app.listen(app.get('port'), function() {
     console.log("listening on port " + app.get('port'));
