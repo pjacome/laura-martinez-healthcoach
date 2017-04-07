265ff2d953a2a56c598cc2df7846caf4b97ee9ca
pablo <pabloejacome@gmail.com>
2017-04-07 14:57:21 -0700

    structure of preview div set up

---
+++ public/js/preview.js
@@ -0,0 +1,20 @@
+﻿/*
+    preview.js
+    For previewing blogs
+*/
+
+function PreviewBlog(blogContent) {
+    console.log('so far so good');
+    $('.preview-div .preview-content').append(blogContent.html());
+
+    $('.preview-div').removeClass('preview-close');
+    $('.preview-div').addClass('preview-expand');
+}
+
+function CloseBlog() {
+    console.log('exiting');
+    $('.preview-div .preview-content').empty();
+
+    $('.preview-div').addClass('preview-close');
+    $('.preview-div').removeClass('preview-expand');
+}
