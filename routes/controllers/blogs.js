59cfa5e7369998f2b1b497d79cef427df0cb34f2
pablo <pabloejacome@gmail.com>
2017-04-12 22:55:19 -0700

    blog uploads and displays are a work in progress

--- routes/controllers/blogs.js
+++ routes/controllers/blogs.js
@@ -22,23 +22,48 @@ module.exports.POST = function(req, res) {
 };
 
 module.exports.GET = function(req, res) {
-    console.log('requesting ALL blogs');
-    db.client.collection('blogs').find({}).toArray(function(err, docs) {
-        if(err) {
-            console.error(err);
-            res.sendStatus(400);
-        } else {
-            console.log(docs);
-            var options = { blogResult: docs };
-            res.render('en/blogs', options);
-        }
-    });
+    // TODO: validation
+    var id = req.params.id;
+    if(id) {
+        var objID = new ObjectID(id);
+        db.client.collection('blogs').find({_id: objID}).toArray(function(err, docs) {
+            if (err) {
+                console.error(err);
+                res.sendStatus(400);
+            } else {
+                console.log('requesting ALL blogs');
+                console.log('Found 1 item:', docs[0]);
+                docs = docs[0];
+                var options = {
+                    title: docs.title,
+                    subheading: docs.subheading,
+                    date: docs.date,
+                    sections: docs.sections
+                };
+                res.render('en/blogs/blogitem', options);
+            }
+        });
+    } else {
+        db.client.collection('blogs').find({}).toArray(function(err, docs) {
+            if (err) {
+                console.error(err);
+                res.sendStatus(400);
+            } else {
+                console.log('requesting ALL blogs');
+                console.log(docs);
+                var options = { blogResult: docs };
+                res.render('en/blogs/list', options);
+            }
+        });
+    }
 };
 
 module.exports.PUT = function(req, res) {
+    // TODO: validation
     res.sendStatus(400);
 };
 
 module.exports.DELETE = function(req, res) {
+    // TODO: validation
     res.sendStatus(400);
 };
